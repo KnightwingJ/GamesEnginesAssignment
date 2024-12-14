5c1925884c369b47112fcecf215acdc220bf8d3d
john hinch <johnhnch864@gmail.com>
2024-12-14 13:31:50 +0000

    New Project

--- gamesenginesassignment/Home.gd
+++ gamesenginesassignment/Home.gd
@@ -14,3 +14,18 @@ func _process(delta):
 func _on_home_pressed():
 	get_tree().change_scene_to_file("res://Sequence.tscn")
 	pass # Replace with function body.
+
+
+func _on_volume_value_changed(value):
+	print("Volume: ",value)
+	pass # Replace with function body.
+
+
+func _on_bpm_value_changed(value):
+	print("BPM: ",value)
+	pass # Replace with function body.
+
+
+func _on_pitch_value_changed(value):
+	print("Pitch: ",value)
+	pass # Replace with function body.
